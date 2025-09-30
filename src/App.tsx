4ea85cc998333d8ab95ff63e4cb225a1663c30cf
YuliaProkipchuk <yulayulayula2003@gmail.com>
2025-09-30 10:01:53 +0300

    update project

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
 import React from 'react';
-import './App.scss';
-
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+import { UserWarning } from './UserWarning';
+import { USER_ID } from './api/todos';
+import HomePage from './components/HomePage';
 
 export const App: React.FC = () => {
-  return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
-    </div>
-  );
+  if (!USER_ID) {
+    return <UserWarning />;
+  }
+
+  return <HomePage />;
 };
